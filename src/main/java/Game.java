913eadf0949e4dacb1e49e26bb2ecbeade743d2b
Leo Cozzens <leo@narthexnetworks.com>
2023-07-02 02:20:04 -0500

    Imported lanterna library and modified build system

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,5 +1,23 @@
+import com.googlecode.lanterna.terminal.*;
+import com.googlecode.lanterna.terminal.ansi.*;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+
 public class Game {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        DefaultTerminalFactory defaultTerminalFactory = new DefaultTerminalFactory();
+        Terminal term = null;
+        try {
+            term = defaultTerminalFactory.createTerminal();
+            term.putCharacter('H');
+            term.putCharacter('e');
+            term.putCharacter('l');
+            term.putCharacter('l');
+            term.putCharacter('o');
+            term.putCharacter('\n');
+            term.flush();
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 }
